080141bd138e6a221411fef57312d359942cb2b2
Mariem Aissa <maissa@equalios.com>
2020-04-03 21:10:08 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import img from './imageInSrc.jpg';
+import './style.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <div style={{border:'solid 1px black',maxWidth:"100vw"}}>
+                  <h1 className="titleRed">Mariem Aissa</h1>  
+                  <br></br>
+                  <img src={img} />  
+                  <br></br>
+                  <img src="/imageInPublic.jpg" style={{width: '630px'}}/>  
+        </div>
+                  <video style={{width: 320,height:240}} controls>  
+                    <source src="myVideo.mp4" type="video/mp4"/> 
+                  </video> 
+
     </div>
+   
+     
   );
 }
 
