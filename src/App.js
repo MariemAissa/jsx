b2b7c35eba5834ede2976e77f3d5a88e3e0ad46f
LABBASSIBassem <49342568+LABBASSIBassem@users.noreply.github.com>
2020-04-10 19:52:24 +0100

    we use <br/> instead of <br> </br>

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ function App() {
     <div>
         <div style={{border:'solid 1px black',maxWidth:"100vw"}}>
                   <h1 className="titleRed">Mariem Aissa</h1>  
-                  <br></br>
+                 <br/>
                   <img src={img} />  
-                  <br></br>
+               <br/>
                   <img src="/imageInPublic.jpg" style={{width: '630px'}}/>  
         </div>
                   <video style={{width: 320,height:240}} controls>  
